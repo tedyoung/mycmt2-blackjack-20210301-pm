1883bb0ac05b05341e64a52168c83632e334cf76
Ted M. Young <tedyoung@gmail.com>
2021-03-03 14:45:15 -0800

    GET to /game calls viewGame method.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -3,6 +3,7 @@ package com.jitterted.ebp.blackjack.adapter.in.web;
 import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,4 +23,8 @@ public class BlackjackController {
     return "redirect:/game";
   }
 
+  @GetMapping("/game")
+  public String viewGame() {
+    return "blackjack";
+  }
 }
